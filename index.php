17a184a0e72abe0f47ff4c8aa4e83971bdbb90b3
puroy3 <168604388+puroy3@users.noreply.github.com>
2024-05-01 00:22:09 +0000

    Testing code

--- index.php
+++ index.php
@@ -1,8 +1,19 @@
+<?php
+
+#Anything here doesn't get rendered on the screen
+$name = "Pushpak"
+?>
+  
 <html>
   <head>
     <title>PHP Test</title>
   </head>
   <body>
     <?php echo '<p>Hello Class</p>'; ?> 
-
+    <?php echo $name;?>
+    <?= $name; ?>
+    <p><?= $name; ?></p>
+    <?= "<p>" .$name. "</p>" ?>
+  </body>
+    
 </html>
